7149020c7bd0cfca57b3ea3869da24edc02e4841
Archit Kashyap <architastitva@gmail.com>
2021-01-12 18:09:18 +0530

    Playing with DOM model

---
+++ DOM/index.js
@@ -0,0 +1,3 @@
+// document.firstElementChild.lastElementChild.querySelector("ul").lastElementChild.innerHTML = "Last";
+// document.getElementsByTagName('li')[2].style.color = "purple";
+// document.querySelector("button").style.backgroundColor = "yellow"
