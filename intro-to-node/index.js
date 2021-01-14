5b073929e54e2361a66be3e84a94180c435ebbca
Archit Kashyap <architastitva@gmail.com>
2021-01-14 22:29:17 +0530

    Intro to Node and npm

---
+++ intro-to-node/index.js
@@ -0,0 +1,10 @@
+var superheros = require("superheroes");
+const supervillains = require("supervillains");
+
+var mySuperheroName = superheros.random();
+console.log(mySuperheroName);
+
+var superVillian = require("supervillains");
+
+var mySupervillianName = supervillains.random();
+console.log(mySupervillianName);
