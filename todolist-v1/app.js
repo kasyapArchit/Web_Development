4c6fcf0c760c7b550623dea08e5de6a00160118c
Archit Kashyap <architastitva@gmail.com>
2021-01-16 20:48:51 +0530

    Multiple to-do list based on category.

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -7,6 +7,7 @@ app.use(express.static("public"));
 app.set('view engine', 'ejs');
 
 let items = ["Buy Food", "Cook Food", "Eat Food"];
+let workItems = [];
 
 // POST and GET functions
 app.get("/", function (req, res) {
@@ -20,14 +21,31 @@ app.get("/", function (req, res) {
 
     let day = today.toLocaleDateString("en-US", options);
 
-    res.render("list", {kindOfDay: day, newListItem: items});
+    res.render("list", {listTitle: day, newListItem: items});
 });
 
 app.post("/", function (req, res) {
-    items.push(req.body.newItem);
-    
-    res.redirect("/");
+    let item = req.body.newItem;
+
+    if(req.body.list === "Work") {
+        workItems.push(item);
+        res.redirect("/work");
+    } else {
+        items.push(item);
+        res.redirect("/");
+    }
 });
+
+app.get("/work", function (req, res) {
+    res.render("list", {listTitle: "Work List", newListItem: workItems});
+});
+
+app.post("/work", function (req, res) {
+    let item = req.body.newItem;
+    workItems.push(item);
+
+    res.redirect("/work");
+})
 
 app.listen(3000, function () {
     console.log("Server up and running at port 3000");
