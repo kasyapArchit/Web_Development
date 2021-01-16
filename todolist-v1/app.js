d5801eb6888bf2535ab1b398b205ee0a1667ecdf
Archit Kashyap <architastitva@gmail.com>
2021-01-16 20:33:18 +0530

    Beautifying with css

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -3,21 +3,22 @@ const bodyParser = require("body-parser");
 
 const app = express();
 app.use(bodyParser.urlencoded({extended:  true}));
+app.use(express.static("public"));
 app.set('view engine', 'ejs');
 
-var items = ["Buy Food", "Cook Food", "Eat Food"];
+let items = ["Buy Food", "Cook Food", "Eat Food"];
 
 // POST and GET functions
 app.get("/", function (req, res) {
 
-    var today = new Date();
-    var options = {
+    let today = new Date();
+    let options = {
         weekday: "long",
         day: "numeric",
         month: "long"
     };
 
-    var day = today.toLocaleDateString("en-US", options);
+    let day = today.toLocaleDateString("en-US", options);
 
     res.render("list", {kindOfDay: day, newListItem: items});
 });
