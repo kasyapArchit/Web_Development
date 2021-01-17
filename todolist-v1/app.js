8eca3a64c1a896b9b65480a5c85716b08f8efc7c
Archit Kashyap <architastitva@gmail.com>
2021-01-17 12:59:18 +0530

    Adding modules to remove repetition.

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -1,26 +1,18 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 app.use(bodyParser.urlencoded({extended:  true}));
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 
-let items = ["Buy Food", "Cook Food", "Eat Food"];
-let workItems = [];
+const items = ["Buy Food", "Cook Food", "Eat Food"];
+const workItems = [];
 
 // POST and GET functions
 app.get("/", function (req, res) {
-
-    let today = new Date();
-    let options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long"
-    };
-
-    let day = today.toLocaleDateString("en-US", options);
-
+    let day = date.getDate();
     res.render("list", {listTitle: day, newListItem: items});
 });
 
