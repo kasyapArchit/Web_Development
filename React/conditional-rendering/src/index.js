50e741a23f52d4ec7193a820129e770da2d66410
Archit Kashyap <architastitva@gmail.com>
2021-01-20 13:20:37 +0530

    Conditional Rendering

---
+++ React/conditional-rendering/src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+// link - https://s3jgs.csb.app/
+// sandbox - https://codesandbox.io/s/conditional-rendering-s3jgs?file=/src/index.js
