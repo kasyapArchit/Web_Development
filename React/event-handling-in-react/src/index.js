22fe339a13972d915914132700fd023c7613fd2c
Archit Kashyap <architastitva@gmail.com>
2021-01-20 18:13:55 +0530

    Event Handling in react

---
+++ React/event-handling-in-react/src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+// sandbox - https://codesandbox.io/s/event-handling-in-react-qhy12?file=/src/index.js
