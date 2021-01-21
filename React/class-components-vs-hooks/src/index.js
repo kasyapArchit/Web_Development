a5a56f4a4be3660c1043254399b146203f9abb59
Archit Kashyap <architastitva@gmail.com>
2021-01-21 11:28:21 +0530

    Class components vs hooks

---
+++ React/class-components-vs-hooks/src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+// sandbox - https://codesandbox.io/s/class-components-vs-hooks-forked-o1kd8?file=/src/index.js
