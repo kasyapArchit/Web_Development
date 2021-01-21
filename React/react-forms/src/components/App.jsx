ba61e7fb85dccf84848ab6fad30ae324307b3ab3
Archit Kashyap <architastitva@gmail.com>
2021-01-21 08:20:32 +0530

    Using forms in react

---
+++ React/react-forms/src/components/App.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+
+function App() {
+  const [name, setName] = useState("");
+  const [headingText, setHeading] = useState("");
+
+  function handleChange(event) {
+    setName(event.target.value);
+  }
+
+  function handleClick() {
+    setHeading(name);
+    event.preventDefault();
+  }
+  
+  return (
+    <div className="container">
+      <h1>Hello {headingText}</h1>
+      <form onSubmit={handleClick}>
+        <input 
+          onChange={handleChange} 
+          type="text" 
+          placeholder="What's your name?" 
+          value={name}
+        />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
---
+++ React/react-forms/src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+
+// sandbox - https://codesandbox.io/s/react-forms-7lfc9?file=/src/index.js
